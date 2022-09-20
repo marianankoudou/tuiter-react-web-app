945edc41e2db3d0824d4b237878eef28c3cba5eb
John Rachlin <john.rachlin@gmail.com>
2022-09-19 21:20:54 -0700

    sankey updates

---
+++ sankey/bio.py
@@ -0,0 +1,15 @@
+"""
+File: bio.py
+Description: Demonstration of the sankey library
+"""
+
+import pandas as pd
+import sankey as sk
+
+
+def main():
+    df = pd.read_csv('bio.csv')
+    sk.make_sankey(df, 'disease', 'gene', 'pubs', line_width=2)
+
+
+main()
